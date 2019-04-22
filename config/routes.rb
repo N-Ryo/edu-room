9dd36efd883cd1c22d5967751f2b92f49ce65a90
N-Ryo <ryo.joy.joy@gmail.com>
2019-04-23 02:56:47 +0900

    first commit and devise & omniauth

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  root to: "home#index"
+  resources :posts
+  devise_scope :user do
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  end
+end
